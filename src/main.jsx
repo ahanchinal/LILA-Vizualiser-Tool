133be9af63e27416beacbd318be478fe46fb56fe
Aditya Hanchinal <aditya.hanchinal@gmail.com>
2026-04-14 00:21:06 +0800

    Add Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode, Component } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import './global.css'
 import App from './App.jsx'
 
@@ -23,6 +24,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ErrorBoundary>
       <App />
+      <Analytics />
     </ErrorBoundary>
   </StrictMode>
 )
